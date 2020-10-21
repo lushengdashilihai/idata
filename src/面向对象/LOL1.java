d52697a158f4c55acc9a39cd0afde870c7bcf3fb
卢升 <508480416@qq.com>
2020-10-21 10:18:48 +0800

    2222

---
+++ src/面向对象/LOL1.java
@@ -0,0 +1,20 @@
+package 面向对象;
+
+public class LOL1 {
+    String name;
+    String name1;
+    String name2;
+    String name3;
+    int num;
+    int sum;
+    int bum;
+    int aum;
+    public void print(){
+        System.out.println("英雄信息:"+"NO.1"+name+"NO.2"+name2+"NO.3"+name3+"NO.4");
+        System.out.println("攻击力属性:"+"魔法伤害"+num+"物理输出"+sum+"混合伤害"+bum+"生命值"+aum);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
